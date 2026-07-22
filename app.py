0ca9c8af1a079e035de591279622dab33f63432d
alirezamohammadzadeh245-star <alirezamohammadzadeh245@gmail.com>
2026-07-23 01:31:06 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask
+from routes import setup_routes
 
 app = Flask(__name__)
+
+setup_routes(app)
 
 @app.route("/")
 def home():
     return "RapBox Bot Online ✅"
 
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    return {"status": "ok"}, 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
